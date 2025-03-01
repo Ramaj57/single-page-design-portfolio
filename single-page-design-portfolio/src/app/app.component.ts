bfa4d1c4706499faa499df047bb567a5e958586f
Ramaj57 <slamaster02@yahoo.com>
2025-03-01 18:51:10 -0500

    initial testing and added config

--- single-page-design-portfolio/src/app/app.component.ts
+++ single-page-design-portfolio/src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { topics } from './data';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'single-page-design-portfolio';
+
+  
 }
---
+++ single-page-design-portfolio/src/app/data.ts
@@ -0,0 +1,8 @@
+export const topics = [
+    // { name: "Graphic Design", image: "/assets/favicon-32x32.png"},
+    // { name: "UI/UX", image: },
+    // { name: "Apps", image: },
+    // { name: "Illustrations", image: },
+    // { name: "Photography", image: },
+    // { name: "Motion Graphics", image: } 
+]
