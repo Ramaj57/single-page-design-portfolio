200dc4bbb19dc3a97f2ecae3f3a5068390441c3a
Ramaj57 <slamaster02@yahoo.com>
2025-03-01 17:44:27 -0500

    added all expected components

---
+++ single-page-design-portfolio/src/app/header-footer/header-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-footer.component.html',
+  styleUrl: './header-footer.component.css'
+})
+export class HeaderFooterComponent {
+
+}
