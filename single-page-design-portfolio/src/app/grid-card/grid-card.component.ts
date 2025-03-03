0850cf02b6b228eb8e4ccc36a33c53dc98d79ad7
Ramaj57 <slamaster02@yahoo.com>
2025-03-02 21:24:58 -0500

    added ngstyle for background color to grid-card component and styled card

--- single-page-design-portfolio/src/app/grid-card/grid-card.component.ts
+++ single-page-design-portfolio/src/app/grid-card/grid-card.component.ts
@@ -1,14 +1,15 @@
-import { Component, input, signal } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-grid-card',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './grid-card.component.html',
   styleUrl: './grid-card.component.css'
 })
 export class GridCardComponent {
 name = input<string>('');
 image = input<string>('');
-backgroundColor = input<string>('');
+gridColor = input<string>('');
 }
