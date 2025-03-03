19aae12ae8a269672e2674011f72003f2aeb10a2
Ramaj57 <slamaster02@yahoo.com>
2025-03-02 21:01:36 -0500

    made grid cards render and got access to data in app component

--- single-page-design-portfolio/src/app/app.component.ts
+++ single-page-design-portfolio/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { HeaderFooterComponent } from './header-footer/header-footer.component';
 import { GridCardComponent } from "./grid-card/grid-card.component";
 import { ResumeCardComponent } from "./resume-card/resume-card.component";
 import { ImageCarouselComponent } from "./image-carousel/image-carousel.component";
 import { ConsultationBtnComponent } from "./consultation-btn/consultation-btn.component";
+import { topics } from './data';
 
 
 @Component({
@@ -14,5 +15,6 @@ import { ConsultationBtnComponent } from "./consultation-btn/consultation-btn.co
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'single-page-design-portfolio';  
+  title = 'single-page-design-portfolio';
+  genre = topics;  
 }
