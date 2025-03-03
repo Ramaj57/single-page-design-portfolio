784b31cad6047ef59ef421101c6d367e6b2f2729
Ramaj57 <slamaster02@yahoo.com>
2025-03-03 07:01:27 -0500

    stopped using ngstyle

--- single-page-design-portfolio/src/app/grid-card/grid-card.component.ts
+++ single-page-design-portfolio/src/app/grid-card/grid-card.component.ts
@@ -4,12 +4,11 @@ import { Component, input } from '@angular/core';
 @Component({
   selector: 'app-grid-card',
   standalone: true,
-  imports: [NgStyle],
+  imports: [],
   templateUrl: './grid-card.component.html',
   styleUrl: './grid-card.component.css'
 })
 export class GridCardComponent {
 name = input<string>('');
 image = input<string>('');
-gridColor = input<string>('');
 }
