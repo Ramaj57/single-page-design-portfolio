ce19412584c7954977f015a029d5711558185466
Ramaj57 <slamaster02@yahoo.com>
2025-03-02 21:01:57 -0500

    fixed image paths

--- single-page-design-portfolio/src/app/data.ts
+++ single-page-design-portfolio/src/app/data.ts
@@ -1,8 +1,32 @@
 export const topics = [
-    { name: "Graphic Design", image: "/assets/pattern-graphic-design.svg"},
-    { name: "UI/UX", image: "/assets/pattern-ui-ux.svg"},
-    { name: "Apps", image: "src/assets/pattern-apps.svg"},
-    { name: "Illustrations", image: "src/assets/pattern-illustrations.svg"},
-    { name: "Photography", image: "src/assets/pattern-photography.svg"},
-    { name: "Motion Graphics", image: "src/assets/pattern-motion-graphics.svg"} 
-]
+  {
+    name: 'Graphic Design',
+    image: '/assets/pattern-graphic-design.svg',
+    backgroundColor: '#755cde',
+  },
+  {
+    name: 'UI/UX',
+    image: '/assets/pattern-ui-ux.svg',
+    backgroundColor: '#f6a560',
+  },
+  {
+    name: 'Apps',
+    image: '/assets/pattern-apps.svg',
+    backgroundColor: '#f39e9e',
+  },
+  {
+    name: 'Illustrations',
+    image: '/assets/pattern-illustrations.svg',
+    backgroundColor: '#eb7565',
+  },
+  {
+    name: 'Photography',
+    image: '/assets/pattern-photography.svg',
+    backgroundColor: '#61c4b7',
+  },
+  {
+    name: 'Motion Graphics',
+    image: '/assets/pattern-motion-graphics.svg',
+    backgroundColor: '#552049',
+  },
+];
