05c948d7ad9f672a058662848db6e04fb112d03a
Ramaj57 <slamaster02@yahoo.com>
2025-03-05 04:19:25 -0500

    added sizeClass property to data

--- single-page-design-portfolio/src/app/data.ts
+++ single-page-design-portfolio/src/app/data.ts
@@ -2,25 +2,31 @@ export const topics = [
   {
     name: 'Graphic Design',
     image: '/assets/pattern-graphic-design.svg',
+    sizeClass: 'graphicDesign'
   },
   {
     name: 'UI/UX',
     image: '/assets/pattern-ui-ux.svg',
+    sizeClass: 'uiUx'
   },
   {
     name: 'Apps',
     image: '/assets/pattern-apps.svg',
+    sizeClass: 'apps'
   },
   {
     name: 'Illustrations',
     image: '/assets/pattern-illustrations.svg',
+    sizeClass: 'illustrations'
   },
   {
     name: 'Photography',
     image: '/assets/pattern-photography.svg',
+    sizeClass: 'photography'
   },
   {
     name: 'Motion Graphics',
     image: '/assets/pattern-motion-graphics.svg',
+    sizeClass: 'motionGraphics'
   },
 ];
