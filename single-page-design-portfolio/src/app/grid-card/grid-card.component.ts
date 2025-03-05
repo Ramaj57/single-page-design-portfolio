3812371696247c32e08e17f4b840626381dc5189
Ramaj57 <slamaster02@yahoo.com>
2025-03-05 04:20:53 -0500

    used ngclass and changed style to size images in grid-cards

--- single-page-design-portfolio/src/app/grid-card/grid-card.component.ts
+++ single-page-design-portfolio/src/app/grid-card/grid-card.component.ts
@@ -1,14 +1,15 @@
-import { NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-grid-card',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './grid-card.component.html',
   styleUrl: './grid-card.component.css'
 })
 export class GridCardComponent {
 name = input<string>('');
 image = input<string>('');
+sizeClass = input<string>('');
 }
